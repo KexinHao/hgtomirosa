9ad6a81e6f5ab3c2d7e74bd17325ed5dc831e3e8
Kexin <kexinhao01111993@gmail.com>
2021-03-17 23:09:03 +0100

    update
    hgtomi rosa typeface frontpage
    image selected cut-out scroll change
    archive page
    each project page

--- js/main.js
+++ js/main.js
@@ -9,29 +9,37 @@ window.addEventListener(
   false
 );
 
-$(document).ready(function(){
-  $("#g1").click(function(){
-    location.href = "html/archive.html";
-  });
-  $("#g2").click(function(){
-    location.href = "html/archive.html";
-  });
-  $("#g3").click(function(){
-    location.href = "html/archive.html";
-  });
-  $("#g4").click(function(){
-    location.href = "html/archive.html";
-  });
-  $("#g5").click(function(){
-    location.href = "html/archive.html";
-  });
-  $("#g6").click(function(){
-    location.href = "html/archive.html";
-  });
-  $("#g7").click(function(){
-    location.href = "html/archive.html";
-  });
-  $("#g8").click(function(){
-    location.href = "html/archive.html";
-  });
-});
+
+
+$(function(){
+     $('.name').click(function(){
+         $('#info-' + $(this).attr('id')).toggle();
+        });
+
+
+    $("#g1").click(function(){
+      location.href = "archive/casestudy.html";
+    });
+    $("#g2").click(function(){
+      location.href = "archive/casestudy.html";
+    });
+    $("#g3").click(function(){
+      location.href = "archive/casestudy.html";
+    });
+    $("#g4").click(function(){
+      location.href = "archive/casestudy.html";
+    });
+    $("#g5").click(function(){
+      location.href = "archive/casestudy.html";
+    });
+    $("#g6").click(function(){
+      location.href = "archive/casestudy.html";
+    });
+    $("#g7").click(function(){
+      location.href = "archive/casestudy.html";
+    });
+    $("#g8").click(function(){
+      location.href = "archive/casestudy.html";
+    });
+
+   })
